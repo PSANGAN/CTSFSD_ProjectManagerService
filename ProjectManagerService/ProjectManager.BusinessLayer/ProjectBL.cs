9e80e3b4827147439222d62ace8f799918b286ed
PSANGAN <PSANGAN@users.noreply.github.com>
2019-06-29 20:56:17 -0400

    Interface and Business changes commited...

---
+++ ProjectManagerService/ProjectManager.BusinessLayer/ProjectBL.cs
@@ -0,0 +1,111 @@
+﻿using ProjectManager.DataAccessLayer;
+using ProjectManager.InterfaceLayer;
+using System.Collections.ObjectModel;
+using System.Data.Entity;
+using System.Linq;
+
+namespace ProjectManager.BusinessLayer
+{
+    public class ProjectBL : IProjectBL
+    {
+        private readonly ProjectManagerEntities _projectManager;
+
+        public ProjectBL()
+        {
+            _projectManager = new ProjectManagerEntities();
+        }
+        public ProjectBL(ProjectManagerEntities projectManager)
+        {
+            _projectManager = projectManager;
+        }
+        public Collection<CommonEntities.Projects> GetProjects()
+        {
+
+            Collection<CommonEntities.Projects> projCollection = new Collection<CommonEntities.Projects>();
+
+            _projectManager.Projects.SelectMany
+            (
+                proj => _projectManager.Users.Where(user => proj.ProjectID == user.ProjectID).DefaultIfEmpty(),
+                (x, y) => new
+                {
+                    Projects = x,
+                    Users = y
+                }
+            ).ToList()
+                .ForEach(y => projCollection.Add(
+                    new CommonEntities.Projects
+                    {
+                        ProjectID = y.Projects.ProjectID,
+                        ProjectName = y.Projects.ProjectName,
+                        StartDate = y.Projects.StartDate,
+                        EndDate = y.Projects.EndDate,
+                        Priority = y.Projects.Priority,
+                        NoofTasks = _projectManager.Tasks.Where(x => x.ProjectID == y.Projects.ProjectID).Count(),
+                        NoofCompletedTasks = _projectManager.Tasks.Where(x => x.ProjectID == y.Projects.ProjectID && x.Status == true).Count(),
+                        ManagerID = y.Users != null ? y.Users.UserID : 0,
+                        ManagerName = y.Users != null ? y.Users.FirstName + " " + y.Users.LastName : ""
+                    }
+                    ));
+
+            return projCollection;
+        }
+
+        public void AddProject(CommonEntities.Projects project)
+        {
+            Project proj = new Project
+            {
+                ProjectName = project.ProjectName,
+                StartDate = project.StartDate,
+                EndDate = project.EndDate,
+                Priority = project.Priority
+            };
+
+            _projectManager.Projects.Add(proj);
+            _projectManager.SaveChanges();
+            var proId = proj.ProjectID;
+            var ur = _projectManager.Users.Where(x => x.UserID == project.ManagerID).FirstOrDefault();
+            if (ur != null)
+            {
+                ur.ProjectID = proId;
+                _projectManager.SaveChanges();
+            }
+        }
+
+        public void UpdateProject(CommonEntities.Projects project)
+        {
+            var proj = _projectManager.Projects.Where(x => x.ProjectID == project.ProjectID).FirstOrDefault();
+            var user = _projectManager.Users.Where(x => x.UserID == project.ManagerID).FirstOrDefault();
+            var extUser = _projectManager.Users.Where(x => x.ProjectID == project.ProjectID).FirstOrDefault();
+
+            if (proj != null && user != null)
+            {
+                proj.ProjectName = project.ProjectName;
+                proj.StartDate = project.StartDate;
+                proj.EndDate = project.EndDate;
+                proj.Priority = project.Priority;
+                if (extUser != null)
+                {
+                    extUser.ProjectID = null;
+                }
+                user.ProjectID = project.ProjectID;
+                _projectManager.SaveChanges();
+            }
+        }
+
+        public void SuspendProject(int projectID)
+        {
+            Project proj = new Project
+            {
+                ProjectID = projectID
+            };
+            var user = _projectManager.Users.Where(x => x.ProjectID == projectID).FirstOrDefault();
+            if (user != null)
+            {
+                user.ProjectID = null;
+            }
+
+            _projectManager.Entry(proj).State = EntityState.Deleted;
+            _projectManager.SaveChanges();
+        }
+    }
+}
---
+++ ProjectManagerService/ProjectManager.BusinessLayer/TaskBL.cs
@@ -0,0 +1,172 @@
+﻿using ProjectManager.DataAccessLayer;
+using ProjectManager.InterfaceLayer;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace ProjectManager.BusinessLayer
+{
+    public class TaskBL : ITaskBL
+    {
+        private readonly ProjectManagerEntities _projectManager;
+
+        public TaskBL()
+        {
+            _projectManager = new ProjectManagerEntities();
+        }
+
+        public TaskBL(ProjectManagerEntities projectManager)
+        {
+            _projectManager = projectManager;
+        }
+
+        public Collection<CommonEntities.ParentTasks> GetParentTasks()
+        {
+
+            Collection<CommonEntities.ParentTasks> taskCollection = new Collection<CommonEntities.ParentTasks>();
+            _projectManager.ParentTasks
+                .Select(u => new CommonEntities.ParentTasks()
+                {
+                    ParentTaskID = u.ParentTaskID,
+                    ParentTaskName = u.ParentTaskName
+                }).ToList()
+               .ForEach(y => taskCollection.Add(y));
+
+            return taskCollection;
+        }
+
+        public Collection<CommonEntities.Tasks> GetTasks(int projectID)
+        {
+
+            Collection<CommonEntities.Tasks> taskCollection = new Collection<CommonEntities.Tasks>();
+
+            _projectManager.Tasks.Where(c => c.ParentTaskID == null && c.ProjectID == projectID)
+                .Select(s => new CommonEntities.Tasks
+                {
+                    TaskName = s.TaskName,
+                    ProjectID = s.ProjectID,
+                    Project = _projectManager.Projects.Where(u => u.ProjectID == s.ProjectID)
+                    .Select(u => u.ProjectName).FirstOrDefault(),
+                    ParentTask = "",
+                    Priority = s.Priority,
+                    StartDate = s.StartDate,
+                    EndDate = s.EndDate,
+                    TaskID = s.TaskID,
+                    Status = s.Status,
+                    UserID = _projectManager.Users.Where(u => u.TaskID == s.TaskID)
+                    .Select(u => u.UserID).FirstOrDefault(),
+                    UserName = _projectManager.Users.Where(u => u.TaskID == s.TaskID)
+                    .Select(u => u.FirstName + " " + u.LastName).FirstOrDefault()
+                }).ToList()
+                    .ForEach(x => taskCollection.Add(x));
+
+            _projectManager.Tasks.Where(c => c.ParentTaskID != null && c.ProjectID == projectID)
+                .Join(_projectManager.ParentTasks, f => f.ParentTaskID, s => s.ParentTaskID,
+                (f, s) => new CommonEntities.Tasks
+                {
+                    TaskName = f.TaskName,
+                    ProjectID = f.ProjectID,
+                    Project = _projectManager.Projects.Where(u => u.ProjectID == f.ProjectID)
+                    .Select(u => u.ProjectName).FirstOrDefault(),
+                    ParentTask = s.ParentTaskName,
+                    Priority = f.Priority,
+                    StartDate = f.StartDate,
+                    EndDate = f.EndDate,
+                    ParentTaskID = s.ParentTaskID,
+                    TaskID = f.TaskID,
+                    Status = f.Status,
+                    UserID = _projectManager.Users.Where(u => u.TaskID == f.TaskID)
+                    .Select(u => u.UserID).FirstOrDefault(),
+                    UserName = _projectManager.Users.Where(u => u.TaskID == f.TaskID)
+                    .Select(u => u.FirstName + " " + u.LastName).FirstOrDefault()
+                }).ToList()
+                 .ForEach(x => taskCollection.Add(x));
+
+            return taskCollection;
+        }
+
+
+        public void AddTask(CommonEntities.Tasks task)
+        {
+            DataAccessLayer.Task tk = new DataAccessLayer.Task
+            {
+                TaskName = task.TaskName,
+                ProjectID = task.ProjectID,
+                Priority = task.Priority,
+                StartDate = task.StartDate,
+                EndDate = task.EndDate,
+                Status = false
+            };
+            if (task.ParentTaskID == 0)
+            {
+                tk.ParentTaskID = null;
+            }
+            else
+            {
+                tk.ParentTaskID = task.ParentTaskID;
+            }
+
+
+            _projectManager.Tasks.Add(tk);
+            _projectManager.SaveChanges();
+            var taskId = tk.TaskID;
+            var ur = _projectManager.Users.Where(x => x.UserID == task.UserID).FirstOrDefault();
+            if (ur != null)
+            {
+                ur.TaskID = taskId;
+                _projectManager.SaveChanges();
+            }
+        }
+
+        public void UpdateTask(CommonEntities.Tasks task)
+        {
+            var tk = _projectManager.Tasks.Where(x => x.TaskID == task.TaskID).FirstOrDefault();
+
+            if (tk != null)
+            {
+                tk.TaskName = task.TaskName;
+                tk.ProjectID = task.ProjectID;
+                tk.Priority = task.Priority;
+                tk.StartDate = task.StartDate;
+                tk.EndDate = task.EndDate;
+                if (task.ParentTaskID == 0)
+                {
+                    tk.ParentTaskID = null;
+                }
+                else
+                {
+                    tk.ParentTaskID = task.ParentTaskID;
+                }
+
+                _projectManager.SaveChanges();
+                var ur = _projectManager.Users.Where(x => x.UserID == task.UserID).FirstOrDefault();
+                if (ur != null)
+                {
+                    ur.TaskID = tk.TaskID;
+                    _projectManager.SaveChanges();
+                }
+            }
+        }
+
+        public void EndTask(CommonEntities.Tasks task)
+        {
+            var tk = _projectManager.Tasks.Where(x => x.TaskID == task.TaskID).FirstOrDefault();
+
+            if (tk != null)
+            {
+                tk.Status = true;
+                _projectManager.SaveChanges();
+            }
+        }
+
+        public void AddParentTask(CommonEntities.ParentTasks pTask)
+        {
+            DataAccessLayer.ParentTask tk = new DataAccessLayer.ParentTask
+            {
+                ParentTaskName = pTask.ParentTaskName
+            };
+
+            _projectManager.ParentTasks.Add(tk);
+            _projectManager.SaveChanges();
+        }
+    }
+}
