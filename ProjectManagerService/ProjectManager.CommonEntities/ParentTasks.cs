a0ee91234bc84f6446bc5832f5cbad7b03519668
PSANGAN <PSANGAN@users.noreply.github.com>
2019-06-29 20:24:33 -0400

    Common Business entities added

---
+++ ProjectManagerService/ProjectManager.CommonEntities/ParentTasks.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjectManager.CommonEntities
+{
+    public class ParentTasks
+    {
+        public int ParentTaskID { get; set; }
+        public string ParentTaskName { get; set; }
+
+    }
+}
---
+++ ProjectManagerService/ProjectManager.CommonEntities/Users.cs
@@ -0,0 +1,12 @@
+﻿namespace ProjectManager.CommonEntities
+{
+    public class Users
+    {
+        public int UserID { get; set; }
+        public int ProjectID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmployeeID { get; set; }
+
+    }
+}
